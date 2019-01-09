0fbc11a27fdb2d04336b63f4c54a4081411e3f31
Jan Nowak <tobiaszbieda@gmail.com>
2019-01-09 16:22:05 +0100

    [tobiasz][register] add logic for checking login length

--- src/main/java/com/example/springtobi/component/RegistrationHelper.java
+++ src/main/java/com/example/springtobi/component/RegistrationHelper.java
@@ -16,11 +16,25 @@ public class RegistrationHelper {
         String logInForCheck = userForRegister.getSecurityData().getLogin();
         String passwordForCheck = userForRegister.getSecurityData().getPassword();
 
+        int dlugloscTegoStringa_czyliLoginu = logInForCheck.length();
+
+        if(dlugloscTegoStringa_czyliLoginu <= 3)
+        {
+            return "gym/registerTobi/lessThenFour";
+        }
+        else
+        {
+            if (dlugloscTegoStringa_czyliLoginu > 10)
+            {
+                return "gym/registerTobi/moreThenTen";
+            }
+        }
         if (allUser.isEmpty())
         {
             dataBase.getUsersList().add(userForRegister);
             return "gym/welcomeTobi";
         }
+
         else
         {
             for (User user : allUser)
