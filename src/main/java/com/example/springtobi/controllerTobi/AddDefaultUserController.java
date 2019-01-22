de9c78f373619f8956f0cec16f22b19164abdb77
Jan Nowak <tobiaszbieda@gmail.com>
2019-01-22 15:07:11 +0100

    [tobiasz][addDefaultUserController] Crap 3

--- src/main/java/com/example/springtobi/controllerTobi/AddDefaultUserController.java
+++ src/main/java/com/example/springtobi/controllerTobi/AddDefaultUserController.java
@@ -1,10 +1,12 @@
 package com.example.springtobi.controllerTobi;
 
 import com.example.springtobi.bean.User;
+import com.example.springtobi.component.AddDefaultUser;
 import com.example.springtobi.component.DataBase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 import java.util.List;
 
@@ -12,18 +14,16 @@ import java.util.List;
 public class AddDefaultUserController
 {
     @Autowired
-    private AddDefaultUserController addDefaultUserController;
-
+    private AddDefaultUser addDefaultUser;
 
 
     @GetMapping("/completedDataBaseNewUsers")
     public String addDefaultUsers()
     {
 
+        String d = this.addDefaultUser.addDefaultUsers();
 
-
-
-        return "gym/welcomeTobi";
+        return d ;
     }
 
 }
