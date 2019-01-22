17069d22504b4c4a9d2769a5c5b4ed7350f747a7
Jan Nowak <tobiaszbieda@gmail.com>
2019-01-22 13:16:18 +0100

    [tobiasz][AddDefaultUserController] Create back bone architecture

--- src/main/java/com/example/springtobi/controllerTobi/AddDefaultUserController.java
+++ src/main/java/com/example/springtobi/controllerTobi/AddDefaultUserController.java
@@ -11,32 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AddDefaultUserController
 {
 
-    @Autowired
-    private DataBase dataBase;
-
 
     @GetMapping("/completedDataBaseNewUsers")
     public String addDefaultUsers()
     {
-            User tobiasz = createUser("tobiasz", "al9a");
-
-
-            this.dataBase.getUsersList().add(createUser("pawel", "a9lb"));
-            this.dataBase.getUsersList().add(tobiasz);
 
 
         return "gym/welcomeTobi";
     }
 
-    private User createUser(String login, String password)
-    {
-        User user = new User();
-
-        user.getSecurityData().setLogin(login);
-        user.getSecurityData().setPassword(password);
-
-        return user;
-    }
 
 
 }
