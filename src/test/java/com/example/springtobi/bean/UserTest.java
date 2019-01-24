a62959d47ea3043a102fff9e1e2259061074509e
Jan Nowak <tobiaszbieda@gmail.com>
2019-01-24 10:42:14 +0100

    [tobiasz][addDefaultUser] I making test Admin

---
+++ src/test/java/com/example/springtobi/bean/UserTest.java
@@ -0,0 +1,44 @@
+package com.example.springtobi.bean;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest
+{
+
+    private User unitTest;
+
+
+    @Before
+    public void setUp()
+    {
+        this.unitTest = new User();
+    }
+
+    @Test
+    public void test()
+    {
+        //given
+
+        //then
+
+        //when
+
+    }
+
+    @Test
+    public void test2()
+    {
+        //given
+
+        //then
+
+        //when
+
+    }
+
+
+
+}
