57d4e7c5d7b52c236de8ce4d86ca4f6579299b95
Jan Nowak <tobiaszbieda@gmail.com>
2019-01-29 14:36:05 +0100

    [tobiasz][LoginController] I make Test and its every think is Good

--- src/test/java/com/example/springtobi/component/LoginHelperTest.java
+++ src/test/java/com/example/springtobi/component/LoginHelperTest.java
@@ -5,6 +5,7 @@ import org.fest.assertions.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.jws.soap.SOAPBinding;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,19 +29,45 @@ public class LoginHelperTest
 
         List<User> usersList = new ArrayList<>();
 
-        User userForTset = new User("tobiasz", "aq12");
+        User userForTest = new User("tobiasz", "aq12");
 
-        User userForTset2 = new User("tobiasz", "aq12");
-        User userForTset3 = new User("12345", "qwer");
+        User userForTest2 = new User("tobiasz", "aq12");
+        User userForTest3 = new User("12345", "qwer");
 
 
         //  when
-       String resalt = this.underTest.checkLogInAndPasswordIsRegister(usersList,userForTset);
+       String resalt = this.underTest.checkLogInAndPasswordIsRegister(usersList,userForTest);
 
         //  then
 
         Assertions.assertThat(resalt).isEqualTo("gym/registerTobi/test2");
 
         }
+
+        @Test
+    public void iExpectedTestBecauseEveryThinkIsCorrect()
+    {
+
+        ////given
+        List<User> userList = new ArrayList<>();
+        User userForAdd = new User("tobiasz","aq12");
+        User userForAdd2 = new User("pawel","aq12");
+        User userForAdd3= new User("12345","aq12w");
+
+        userList.add(userForAdd);
+        userList.add(userForAdd2);
+        userList.add(userForAdd3);
+
+        User userForTheTest = new User("12345", "aq12w");
+
+        ////when
+
+        String result = this.underTest.checkLogInAndPasswordIsRegister(userList, userForTheTest);
+
+        ////then
+
+        Assertions.assertThat(result).isEqualTo("gym/registerTobi/test");
+
+    }
 
 }
